1d049a6dd94e3f85050f3c2d77367f78275c9334
Sophie <sallysophie@hotmail.de>
2024-08-21 17:08:36 +0200

    done

--- src/App.jsx
+++ src/App.jsx
@@ -5,44 +5,106 @@ import TableHeader from "./components/TableHeader";
 import StudentCard from "./components/StudentCard";
 
 import studentsData from "./assets/students.json";
+import AddMovie from "./components/AddStudent";
 
 function App() {
   const [students, setStudents] = useState(studentsData);
+  const [fullName, setFullName] = useState("");
+  const [image, setImage] = useState("");
+  const [phone, setPhone] = useState("");
+  const [email, setEmail] = useState("example@email.com");
+  const [program, setProgram] = useState("-- None --");
+  const [graduation, setGraduation] = useState("");
+  const [graduationYear, setGraduationYear] = useState(2024);
+  const [graduated, setGraduated] = useState(false);
 
+  const addStudent = (newStudent) => {
+    const updatedStudents = [...studentsData, newStudent];
+
+    setStudents(updatedStudents);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const newStudent = {
+      fullName,
+      image,
+      phone,
+      email,
+      program,
+      graduationYear,
+      graduated,
+    };
+
+    addStudent(newStudent);
+
+    setFullName("");
+    setImage("");
+    setPhone("");
+    setEmail("");
+    setProgram("-- None --");
+    setGraduationYear(2024);
+    setGraduated(false);
+  };
 
   return (
     <div className="App pt-20">
       <Navbar />
-
+      <addStudent addStudent={addStudent} />
       {/* FORM */}
-      <form>
+      <form onSubmit={handleSubmit}>
         <span>Add a Student</span>
         <div>
           <label>
             Full Name
-            <input name="fullName" type="text" placeholder="Full Name" />
+            <input
+              name="fullName"
+              type="text"
+              value={fullName}
+              onChange={(e) => setFullName(e.target.value)}
+            />
           </label>
 
           <label>
             Profile Image
-            <input name="image" type="url" placeholder="Profile Image" />
+            <input
+              name="image"
+              type="text"
+              value={image}
+              onChange={(e) => setImage(e.target.value)}
+            />
           </label>
 
           <label>
             Phone
-            <input name="phone" type="tel" placeholder="Phone" />
+            <input
+              name="phone"
+              type="number"
+              value={phone}
+              onChange={(e) => setPhone(e.target.value)}
+            />
           </label>
 
           <label>
             Email
-            <input name="email" type="email" placeholder="Email" />
+            <input
+              name="email"
+              type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
           </label>
         </div>
 
         <div>
           <label>
             Program
-            <select name="program">
+            <select
+              id="program"
+              value={program}
+              onChange={(e) => setProgram(e.target.value)}
+            >
               <option value="">-- None --</option>
               <option value="Web Dev">Web Dev</option>
               <option value="UXUI">UXUI</option>
@@ -55,29 +117,32 @@ function App() {
             <input
               name="graduationYear"
               type="number"
-              placeholder="Graduation Year"
+              value={graduationYear}
               minLength={4}
               maxLength={4}
               min={2023}
               max={2030}
+              onChange={(e) => setGraduationYear(e.target.value)}
             />
           </label>
 
           <label>
             Graduated
-            <input name="graduated" type="checkbox" />
+            <input
+              name="graduated"
+              type="checkbox"
+              checked={graduated}
+              onChange={(e) => setGraduated(e.target.checked)}
+            />
           </label>
 
           <button type="submit">Add Student</button>
         </div>
-
       </form>
       {/* FORM END */}
 
-
       {/* TABLE/LIST HEADER */}
       <TableHeader />
-
 
       {/* STUDENT LIST */}
       {students &&
---
+++ src/components/AddStudent.jsx
@@ -0,0 +1,19 @@
+import data from "../assets/students.json";
+
+export default function AddMovie(props) {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newStudent = {
+      name,
+      image,
+      phone,
+      email,
+      program,
+      graduation,
+      graduationYear,
+      gratuated,
+    };
+
+    console.log("Submitted ", newStudent);
+  };
+}
